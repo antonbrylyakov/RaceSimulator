2d8a0831f744a1cb55f68f942c36a4312443bb47
Anton Brylyakov <yoll@rambler.ru>
2022-10-22 22:48:09 -0700

    Кентавр и Ботинки-вездеходы

---
+++ RaceSimulation/AllTerrainBoots.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "GroundVehicle.h"
+
+class AllTerrainBoots : public GroundVehicle
+{
+public:
+    AllTerrainBoots();
+
+protected:
+    virtual double getRestTimeHr(const int distanceKm) const override;
+private:
+};
---
+++ RaceSimulation/Centaur.cpp
@@ -0,0 +1,13 @@
+﻿#include "Centaur.h"
+
+Centaur::Centaur()
+{
+    _name = "Кентавр";
+    _velocityKmh = 15;
+    _motionTimeBeforeRestHr = 8;
+}
+
+double Centaur::getRestTimeHr(const int distanceKm) const
+{
+    return 8 * getRestNumber(distanceKm);
+}
